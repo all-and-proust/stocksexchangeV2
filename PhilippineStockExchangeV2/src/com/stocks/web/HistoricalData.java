57608038fba1b5409c66a5bc8a54abe178acbe6e
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2016-07-13 23:01:22 +0800

    Philippine Stock Exchange Java project Version 2. This is for 3 percent ROI.

---
+++ PhilippineStockExchangeV2/src/com/stocks/web/HistoricalData.java
@@ -0,0 +1,28 @@
+package com.stocks.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import com.stocks.model.dao.StocksDao;
+import com.stocks.model.dao.impl.StocksDaoImpl;
+import java.util.Map;
+
+public class HistoricalData extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+		
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		StocksDao ss = new StocksDaoImpl();
+		Map<String,Object> historicalDataMap = ss.getHistoricalData(request.getParameter("symbol"));
+		//out.println("Stock Symbol: " + );		
+		//for(Stock s: stocks){
+		//	out.println("<br><br>Stock Symbol: " + s.getStockSymbol());
+		//	out.println("<br>Stock Frequency: " + s.getFrequency());
+		//}
+		request.setAttribute("historicalMap",historicalDataMap);
+		RequestDispatcher view = request.getRequestDispatcher("HistoricalData.jsp");
+		view.forward(request,response);
+	}
+}
